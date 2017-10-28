6e764cbf1e8abf0dbef2103ac8f9f5becb39c0e8
nusspez <nusspez@gmail.com>
2017-10-28 14:21:10 -0500

    Delete tokens.py

--- tokens.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['CONSUMER_KEY']			= "6JYNGJZjLZqm6GufIm2jPFRjS"
-os.environ['CONSUMER_SECRET']		= "dMKDAXJJd1SdbO1qKqZ1qhYMk4cHDukArqYwaIKkMAPBXMwIgs"
-os.environ['ACCESS_TOKEN']			= "459802483-rI0tp9wXRj7T9vMQyA04Qfoai3mivunja45wt2Dq"
-os.environ['ACCESS_TOKEN_SECRET']	= "FxilKKP7J4eKRfD4VIMrTcoE8gQhKJS7XquPD550792XQ" 
