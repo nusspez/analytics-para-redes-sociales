2c54951fcb973eaa26f657274e3dafddc0627de5
Oswaldo Cruz <oswaldo_cs_94@hotmail.com>
2017-10-29 00:57:29 -0500

    add script for retweeting by id

--- retweet_example.py
+++ retweet_example.py
@@ -7,28 +7,10 @@ consumer_secret = os.environ['CONSUMER_SECRET']
 access_token    = os.environ['ACCESS_TOKEN']
 access_secret   = os.environ['ACCESS_TOKEN_SECRET']
 
-
 # Access and authorize our Twitter credentials from credentials.py
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
-
-for tweet in tweepy.Cursor(api.search, q='#ocean').items():
-    try:
-        # Add \n escape character to print() to organize tweets
-        print('\nTweet by: @' + tweet.user.screen_name)
-        print(tweet)
-        print(dir(tweet))
-        # Retweet tweets as they are found
-        #tweet.retweet()
-        print('Retweeted the tweet')
-        print ("_____________________________________________________")
-
-        sleep(5)
-
-    except tweepy.TweepError as e:
-        print(e.reason)
-
-    except StopIteration:
-        break
+api.retweet("924270051598979073")
+print('Retweeted the tweet')
