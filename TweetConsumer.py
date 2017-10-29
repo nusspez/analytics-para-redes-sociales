0d757341654f1ceb4c4d37a12b19b4f7e8c5fccd
Oswaldo Cruz <oswaldo_cs_94@hotmail.com>
2017-10-28 22:58:10 -0500

    change name

---
+++ TweetConsumer.py
@@ -0,0 +1,32 @@
+from kafka import KafkaConsumer
+from ElasticSearchClient import ElasticSearchClient
+from sendMongo import SendMongo
+import collections,json
+import numpy as np
+
+
+
+class TweetConsumer:
+	def __init__(self,urlKafka,topic,urles,urlMongo):
+		self.urlKafka = urlKafka
+		self.topic = topic
+		self.consumer = KafkaConsumer(self.topic,bootstrap_servers=[self.urlKafka])
+		self.urles = urles
+		self.es = ElasticSearchClient(urles)
+		self.urlMongo = urlMongo
+		self.mongo = SendMongo(urlMongo)
+	def receiveMessage(self):
+		for message in self.consumer:
+			tweet = json.loads(message.value.decode('utf8'))
+			text = tweet["text"]
+			iddoc = str( int(message.key))
+			#self.es.sendTweet('oswaldo','tweet',iddoc,{'text':text})
+			#self.mongo.saveTweet("oswaldo",'tweet',iddoc,tweet)
+			print(tweet)
+
+urles = "localhost:9200"
+urlMongo = "54.174.5.92:27017"
+urlKafka = "localhost:9092"
+topic = "tweet"
+consumer = TweetConsumer(urlKafka,topic,urles,urlMongo)
+consumer.receiveMessage()
